a7c9aae782f9465fb1463039869a6381fe39b65f
dalipkumar703 <dalipkumar703@gmail.com>
2020-11-17 00:06:53 +0530

    feat(search): add search in map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {Map as LeafMap, Marker, Popup, TileLayer } from 'react-leaflet'
 import {forEach, findIndex} from 'lodash'
 import {Form, Row, Col, Container} from 'react-bootstrap'
+import { InputSearch, Popover, List, Button,ListItem } from '@momentum-ui/react';
 
 import recycleShopConstant from './recycleShopConstant'
 import {sustainableShopsConstant, getIcon} from './sustainableShopConstant'
 import recycleShopsConstants from './recycleShopConstant'
 import {getLocationDetailByGeoCodes} from './mapApiHelper';
 import PopupComponent from './PopupComponent';
+import SearchComponent from './SearchComponent';
+import {filterShopsByType} from './sustainableShopConstant';
+import uniqueId from 'lodash/uniqueId';
+
 
  const centerPosition = [28.62709, 77.05001];
 
 const App = () => {
   const [recycleShopEnable, setRecycleShopEnable] = useState(false);
   const [sustainableShopEnable, setSustainableShopEnable] = useState(false);
+  const [searchListA, setSearchListA] = useState([]);
+  const [displayShops, setDisplayShops] = useState(false);
   const [currentLocation, setCurrentLocation] = useState(null);
   const [markersForSustainableShops, setMarkersForSustainableShops] = useState(null);
+  const [markersForDisplayShops, setMarkersForDisplayShops] = useState(null);
+  const [searchComponentVisibile, setSearchComponentVisible] = useState(true);
+  const [clearButton, setClearButton] = useState(false);
+  const markerMapRef = useRef();
   let markersForRecycleShops = [];
   const shopDetailsMap = new Map();
 
+//   const handleOnClick = (e) => {
+//     if (e.target.id){
+//         const geodata = filterShopsByType(e.target.id);
+//         console.log("geodata:",geodata);
+//         forEach(geodata, (geoCoordinate) => {
+//           const result = JSON.parse(localStorage.getItem(`${geoCoordinate[0][0]}_${geoCoordinate[0][1]}`));
+//         searchList.push(<ListItem>{`${result.name}\n${result.display_name}`}</ListItem>)
+//         })
+//     }
+// };
+const displayMarker = (pos, type, result) => (
+<Marker position={pos} icon={getIcon(type)} ref={markerMapRef}>
+         <PopupComponent shopDetailParser={JSON.parse(result)} />
+        </Marker>
+);
+const handleSearchKeyDown = (e) => {
+  if (e.target.innerText || e.keyCode === 13){
+    let storageLength = localStorage.length;
+    while (storageLength > 0){
+     const result = localStorage.getItem(localStorage.key(storageLength - 1));
+     if (result && JSON.parse(result)?.display_name?.indexOf(e.target.innerText) > -1){
+      const displayContent = displayMarker(localStorage.key(storageLength -1 ).split("_"), e.target.id, result);
+     
+      setMarkersForDisplayShops(displayContent);
+      setDisplayShops(true);
+    }
+    storageLength = storageLength - 1;
+    }
+  }
+};
+
+useEffect(()=> {
+  if (displayShops) {
+  markerMapRef.current.leafletElement.openPopup();
+  }
+}, [displayShops])
+useEffect(()=>{
+  if (searchListA.length > 0){
+    setSearchComponentVisible(false);
+    setClearButton(true);
+  }
+}, [searchListA])
+const buttonComponent = (
+  <Button
+  children='Clear'
+  ariaLabel='Clear'
+  onClick={()=>{
+    setSearchListA([]);
+    setSearchComponentVisible(true);
+    setClearButton(false);
+  }}
+/>
+);
+const searchListComponent = (
+     <List onClick={(e) => {
+  if (e.target.value && e.keyCode === 13){
+    let storageLength = localStorage.length;
+    while (storageLength > 0){
+     const result = localStorage.getItem(localStorage.key(storageLength - 1));
+     if (result && result.display_name.indexOf(e.target.value) > -1){
+      const displayContent = displayMarker(localStorage.key(storageLength -1 ).split("_"), e.target.id, result);
+     
+      setMarkersForDisplayShops(displayContent);
+      setDisplayShops(true);
+      storageLength = storageLength - 1;
+    }
+    }
+  }
+}}>
+      {searchListA}
+    </List>
+   
+  );
   useEffect(()=>{
     if ('geolocation' in navigator){
       navigator.geolocation.getCurrentPosition(function(position) {
@@ -67,7 +151,7 @@ const App = () => {
   const handleOnClickRecycleShop = (event) => {
    setRecycleShopEnable(event.target.checked)
   }
-
+//TODO: add recycle shops
   (forEach(recycleShopConstant, (position) =>{
     markersForRecycleShops.push( <Marker position={position} >
      <Popup>
@@ -85,10 +169,57 @@ const App = () => {
     <Container>
     <Form>
       <Row>
-       <div>Tick to view recyle or sustainable shops around you.</div>
+       <div>Click/Tick to view recyle or sustainable shops around you.</div>
       </Row>
       <Row>
-        
+      <Popover
+          content={searchListComponent}
+          // autoFocusOnFirstElt
+          showArrow={false}
+          popoverTrigger={'MouseEnter'}
+          direction={'right-center'}
+          style={{marginTop: '50px', zIndex: 1085}}
+          // onKeyDown={handleKeyDown}
+          // onChange={handleSearchKeyDown}
+          closeOnClick={true}
+          close={handleSearchKeyDown}
+          // anchorNode={searchListComponent}
+          allowClickAway={false}
+          // startOpen={searchListA.length > 0? true : false}
+        >
+          
+ <InputSearch
+      clear
+      htmlId='pillSearchInput'
+      containerSize='medium-6'
+      name='pillSearchInput'
+      shape='pill'
+      className={{padding: '5px'}}
+    />
+            </Popover>
+            {clearButton && buttonComponent}
+                <SearchComponent isVisible={searchComponentVisibile} onClick={(e) => {
+    if (e.target.id){
+        const geodata = filterShopsByType(e.target.id);
+        const searchList = [];
+
+        console.log("geodata:",geodata);
+        forEach(geodata, (geoCoordinate) => {
+          console.log("data:", localStorage.getItem(`${geoCoordinate[0][0]}_${geoCoordinate[0][1]}`))
+          const result = JSON.parse(localStorage.getItem(`${geoCoordinate[0][0]}_${geoCoordinate[0][1]}`));
+          if (result){
+            console.log("data result:",result)
+            searchList.push(<ListItem key={uniqueId()} id={e.target.id}>{`${result.display_name}`}</ListItem>)
+          }
+        })
+        setSearchListA(searchList);
+       const activeElement = document.getElementById('pillSearchInput');
+       if (activeElement){
+         activeElement.focus();
+       }
+    }
+}}/>
+     
         <Col><Form.Check type="checkbox">
         <Form.Check.Input onClick={handleOnClickSustainbleShop} type="checkbox" isValid />
         <Form.Check.Label>Sustainable Shop</Form.Check.Label>
@@ -108,6 +239,7 @@ const App = () => {
       />
       {recycleShopEnable && markersForRecycleShops}
       {sustainableShopEnable && markersForSustainableShops}
+      {displayShops && markersForDisplayShops}
     </LeafMap>
     </Container>
   )
---
+++ src/SearchComponent.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Container, Row, Col, Image} from 'react-bootstrap';
+import {EventOverlay} from '@momentum-ui/react'
+import {PropTypes} from 'prop-types';
+
+const SearchComponent = ({onClick, isVisible}) => {
+    const content = isVisible ?  <EventOverlay
+    isOpen={true}
+    style={{marginTop: '40px'}}
+    allowClickAway={false}
+    >
+<Row>
+    <Col xs={3} md={4}>
+        <a  onClick={onClick}>        <Image src="https://svgur.com/i/Qei.svg" id="ORGANIC_COSMETICS" height="30px" width="30px" fluid />
+</a>
+    <br></br>
+    <p>Cometics</p>
+  </Col>
+  <Col xs={3} md={4}>
+    {' '}
+    <a  onClick={onClick}>   <Image src="https://svgur.com/i/QJz.svg" id="ORGANIC_VEGETABLE" height="30px" width="30px" fluid /></a>
+    <br></br>
+    <p>Vegetables</p>
+  </Col>
+  <Col xs={3} md={4}>
+    {' '}
+    <a  onClick={onClick}>   <Image src="https://svgur.com/i/QHz.svg" id="SUSTAINABLE_PADS" height="30px" width="30px" fluid /></a>
+    <br></br>
+    <p>Pads</p>
+  </Col>
+    </Row>
+    <Row>
+    <Col xs={3} md={4}>
+    {' '}
+    <a  onClick={onClick}>   <Image src="https://svgur.com/i/QKk.svg" id="ECO_FRIENDLY_PRODUCTS" height="30px" width="30px" fluid /></a>
+    <br></br>
+    <p>Eco Products</p>
+  </Col>
+  <Col xs={3} md={4}>
+    {' '}
+    <a  onClick={onClick}>   <Image src="https://svgur.com/i/R7a.svg" id="GREEN_GIFT" height="30px" width="30px" fluid /></a>
+    <br></br>
+    <p>Gift</p>
+  </Col>
+  <Col xs={3} md={4}>
+    {' '}
+    <a  onClick={onClick}>     <Image src="https://svgur.com/i/R2S.svg" id="ORGANIC_MEAT" height="30px" width="30px" fluid /></a>
+    <br></br>
+    <p>Plant meat</p>
+  </Col>
+    </Row>
+    </EventOverlay>: null;
+    return content;
+};
+SearchComponent.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    isVisible: PropTypes.bool.isRequired
+  };
+
+export default SearchComponent;
