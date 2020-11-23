96e283de5d5ed99d4e6f72b5854efe34e48579cb
dalipkumar703 <dalipkumar703@gmail.com>
2020-11-23 22:02:54 +0530

    feat(searchcomponent): update grid

--- src/SearchComponent.js
+++ src/SearchComponent.js
@@ -9,61 +9,64 @@ const SearchComponent = ({onClick, isVisible}) => {
     style={{marginTop: '40px'}}
     allowClickAway={false}
     >
+      <Container>
 <Row>
-    <Col xs={3} md={4}>
+    <Col xs={3} md={3}>
         <a  onClick={onClick}>        <Image src="https://svgur.com/i/Qei.svg" id="ORGANIC_COSMETICS" height="30px" width="30px" fluid />
 </a>
     <br></br>
     <p>Cometics</p>
   </Col>
-  <Col xs={3} md={4}>
+  <Col xs={3} md={3}>
     {' '}
     <a  onClick={onClick}>   <Image src="https://svgur.com/i/QJz.svg" id="ORGANIC_VEGETABLE" height="30px" width="30px" fluid /></a>
     <br></br>
     <p>Vegetables</p>
   </Col>
-  <Col xs={3} md={4}>
+  <Col xs={3} md={3}>
     {' '}
     <a  onClick={onClick}>   <Image src="https://svgur.com/i/QHz.svg" id="SUSTAINABLE_PADS" height="30px" width="30px" fluid /></a>
     <br></br>
     <p>Pads</p>
   </Col>
-    </Row>
-    <Row>
-    <Col xs={3} md={4}>
+  <Col xs={3} md={3}>
     {' '}
     <a  onClick={onClick}>   <Image src="https://svgur.com/i/QKk.svg" id="ECO_FRIENDLY_PRODUCTS" height="30px" width="30px" fluid /></a>
     <br></br>
     <p>Eco Products</p>
   </Col>
-  <Col xs={3} md={4}>
+    </Row>
+    <Row>
+   
+  <Col xs={3} md={3}>
     {' '}
     <a  onClick={onClick}>   <Image src="https://svgur.com/i/R7a.svg" id="GREEN_GIFT" height="30px" width="30px" fluid /></a>
     <br></br>
     <p>Gift</p>
   </Col>
-  <Col xs={3} md={4}>
+  <Col xs={3} md={3}>
     {' '}
     <a  onClick={onClick}>     <Image src="https://svgur.com/i/R2S.svg" id="ORGANIC_MEAT" height="30px" width="30px" fluid /></a>
     <br></br>
     <p>Plant meat</p>
   </Col>
-    </Row>
-    <Row>
-    <Col xs={3} md={4}>
+  <Col xs={3} md={3}>
     {' '}
     <a  onClick={onClick}>   <Image src="https://svgur.com/i/RfN.svg" id="ORGANIC_ICECREAM" height="60px" width="60px" fluid /></a>
     <br></br>
     <p>Icecream</p>
   </Col>
-  <Col xs={3} md={4}>
+  <Col xs={3} md={3}>
     {' '}
     <a  onClick={onClick}>   <Image src="https://svgur.com/i/Reb.svg" id="ORGANIC_FASTFOOD" height="70px" width="60px" fluid /></a>
     <br></br>
     <p>Fast Food</p>
   </Col>
     </Row>
-    </EventOverlay>: null;
+   
+    </Container>
+    </EventOverlay>
+    : null;
     return content;
 };
 SearchComponent.propTypes = {
